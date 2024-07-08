e56e74783a0752dd1759f76530caa8b15ba6593c
Denys Hrushkevych <flyingzaptop@gmail.com>
2024-07-08 18:59:10 +0200

    task2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-print("Hello, World!")
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-number = int(input("Enter a number to calculate its factorial: "))
-print(f"The factorial of {number} is {factorial(number)}")
+number = int(input("Enter a number to check if it is prime: "))
+print(f"{number} is prime: {is_prime(number)}")
