64cc67b4c082ff9bdced5117fc5b9214d81c5a62
Denys Hrushkevych <flyingzaptop@gmail.com>
2024-07-08 18:55:34 +0200

    main change commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello, World!")
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = int(input("Enter a number to calculate its factorial: "))
+print(f"The factorial of {number} is {factorial(number)}")
